0423b8692dd1b932ba51bbfd6ae0762956c44c1e
BlueCitron <sds901234@naver.com>
2019-12-18 00:04:04 +0900

    PM2 설정

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  apps : [{
+    name: 'CRAWLING_OKKY',
+    script: './src/index.js',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    log_file: path.resolve(__dirname, 'log/app.log'),
+    error_file: path.resolve(__dirname, 'log/error.log'),
+    time: true,
+    env: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
