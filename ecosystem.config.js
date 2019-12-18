b816ffce9a46b59bf428b3d205c2bedf0d4c6b4f
BlueCitron <sds901234@naver.com>
2019-12-18 22:47:13 +0900

    econsystem -> autorestart, watch -> set

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
     script: './src/index.js',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
-    autorestart: false,
-    watch: false,
+    autorestart: true,
+    watch: true,
     max_memory_restart: '1G',
     log_file: path.resolve(__dirname, 'log/app.log'),
     error_file: path.resolve(__dirname, 'log/error.log'),
